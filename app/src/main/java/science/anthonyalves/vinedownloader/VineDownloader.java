a1d216b766fb9beb3893f534787d080dd665f5df
Anthony Alves <aalves2012@my.fit.edu>
2015-12-18 17:27:50 -0500

    updated for vine 3.3.15
    vine revamped their code with FeedAdapter v2

--- app/src/main/java/science/anthonyalves/vinedownloader/VineDownloader.java
+++ app/src/main/java/science/anthonyalves/vinedownloader/VineDownloader.java
@@ -16,13 +16,17 @@ import android.widget.Toast;
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
 
 public class VineDownloader implements IXposedHookLoadPackage {
 
@@ -119,24 +123,37 @@ public class VineDownloader implements IXposedHookLoadPackage {
                     param.setResult(false);
                 }
 
+
                 /*
                     BaseTimelineFragment has (FeedAdapter) mFeedAdapter
-                        FeedAdapter has (FeedAdapterPosts) mPosts
-                            FeedAdapterPosts has (ArrayList<VinePost>) mPosts
+                        FeedAdapter has (FeedAdapterItems) mItems
+                            FeedAdapterPosts has (ArrayList<TimelineItem>) mItems
                  */
                 Object mFeedAdapter = XposedHelpers.getObjectField(param.thisObject, "mFeedAdapter");
-                Object mFeedAdapterPosts = XposedHelpers.getObjectField(mFeedAdapter, "mPosts");
-                ArrayList<?> vinePostArray = (ArrayList) XposedHelpers.getObjectField(mFeedAdapterPosts, "mPosts");
+                ArrayList<?> vinePostArray = null;
 
-                Object vinePost = null;
+                final Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+                final Context context = (Context) callMethod(activityThread, "getSystemContext");
+                final String appVersionString = context.getPackageManager().getPackageInfo(PACKAGE_NAME, 0).versionName;
+
+                if (versionCompare("3.3.15", appVersionString) > 0) { //versions under 3.3.15 aka, old feedadapter
+                    Object mFeedAdapterPosts = XposedHelpers.getObjectField(mFeedAdapter, "mPosts");
+                    vinePostArray = (ArrayList) XposedHelpers.getObjectField(mFeedAdapterPosts, "mPosts");
+                } else {
+                    // version 1.0.1 vine 3.3.15 - FeedAdapterPosts is now the FeedAdapterItems object
+                    // mPosts -> mItems
+                    Object mFeedAdapterPosts = XposedHelpers.getObjectField(mFeedAdapter, "mItems");
+                    vinePostArray = (ArrayList) XposedHelpers.getObjectField(mFeedAdapterPosts, "mItems");
+                }
 
                 if (vinePostArray == null) {
                     param.setResult(false);
                 }
 
-                Iterator it = vinePostArray.iterator();
-                while (it.hasNext()) {
-                    Object obj = it.next();
+                Object vinePost = null;
+
+                assert vinePostArray != null;
+                for (Object obj : vinePostArray) {
                     if (XposedHelpers.getLongField(obj, "postId") == postId) {
                         vinePost = obj;
                         break;
@@ -147,7 +164,10 @@ public class VineDownloader implements IXposedHookLoadPackage {
                     param.setResult(false);
                 }
 
+                assert vinePost != null;
                 String videoUrl = (String) XposedHelpers.getObjectField(vinePost, "videoUrl");
+                //XposedBridge.log("videoUrl: " + videoUrl);
+
                 if (videoUrl != null && videoUrl.isEmpty()) {
                     videoUrl = (String) XposedHelpers.getObjectField(vinePost, "videoLowURL");
 
@@ -163,9 +183,10 @@ public class VineDownloader implements IXposedHookLoadPackage {
                 if (!directory.exists()) {
                     directory.mkdirs();
                 }
+
                 Request request = new Request(Uri.parse(videoUrl));
                 request.setDescription(description);
-                request.setTitle("Vine Video");
+                request.setTitle(description);
                 request.allowScanningByMediaScanner();
                 request.setNotificationVisibility(1);
                 request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Vine/" + postId + ".mp4");
@@ -173,5 +194,44 @@ public class VineDownloader implements IXposedHookLoadPackage {
                 param.setResult(false);
             }
         });
+    }
+
+    /**
+     * http://stackoverflow.com/a/6702029
+     * Compares two version strings.
+     *
+     * Use this instead of String.compareTo() for a non-lexicographical
+     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+     *
+     * @note It does not work if "1.10" is supposed to be equal to "1.10.0".
+     *
+     * @param str1 a string of ordinal numbers separated by decimal points.
+     * @param str2 a string of ordinal numbers separated by decimal points.
+     * @return The result is a negative integer if str1 is _numerically_ less than str2.
+     *         The result is a positive integer if str1 is _numerically_ greater than str2.
+     *         The result is zero if the strings are _numerically_ equal.
+     */
+    public Integer versionCompare(String str1, String str2)
+    {
+        String[] vals1 = str1.split("\\.");
+        String[] vals2 = str2.split("\\.");
+        int i = 0;
+        // set index to first non-equal ordinal or length of shortest version string
+        while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i]))
+        {
+            i++;
+        }
+        // compare first non-equal ordinal number
+        if (i < vals1.length && i < vals2.length)
+        {
+            int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+            return Integer.signum(diff);
+        }
+        // the strings are equal or one string is a substring of the other
+        // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+        else
+        {
+            return Integer.signum(vals1.length - vals2.length);
+        }
     }
 }
